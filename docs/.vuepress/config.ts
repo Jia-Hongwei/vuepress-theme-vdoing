604f3f6d90751a5e8ff220363426e678d4f68a3d
jiahongwei <1095514902@qq.com>
2023-12-14 16:44:06 +0800

    新增文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -115,7 +115,16 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/lf.jpeg', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: 'Jia-Hongwei/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: '查看源码',
+    // 以下为可选的编辑链接选项
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: 'vuejs/vuepress',
+    // 假如文档不是放在仓库的根目录下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
